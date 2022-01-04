80827235dc07fd6847a363c3002abecc9a785237
Luca861 <luca.carapellese@gmail.com>
2022-01-05 00:08:47 +0100

    update carousel

--- src/app/modules/devel-anime/components/carousel/carousel.component.ts
+++ src/app/modules/devel-anime/components/carousel/carousel.component.ts
@@ -1,9 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-
-import { IPartialAnimeManga } from 'src/app/shared/models/interfaces-models';
-import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-carousel',
@@ -11,13 +6,13 @@ import { ApiService } from '../../services/api.service';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+imageOne: string = 'https://img.wallpapersafari.com/desktop/1920/1080/48/59/J8faFX.jpg';
+imageTwo: string = 'https://3.bp.blogspot.com/-Yt0TlLAen40/XSRKIEy-85I/AAAAAAAAAjs/_rZ0iVQWkqAPVmyutfNTKjVcA4_RLtA_QCKgBGAs/w1920-h1080-c/mikasa-levi-colossal-titan-attack-on-titan-uhdpaper.com-8K-160.jpg';
+imageThree: string = 'https://www.ammazzacaffe.org/wp-content/uploads/2021/05/violetevergarden_s.jpg';
 
-  testOne$!: Observable<IPartialAnimeManga[]>;
-
-  constructor(private readonly api: ApiService, private readonly http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.testOne$ = this.api.getApi('anime', 'Tsubasa');
   }
 
 }
